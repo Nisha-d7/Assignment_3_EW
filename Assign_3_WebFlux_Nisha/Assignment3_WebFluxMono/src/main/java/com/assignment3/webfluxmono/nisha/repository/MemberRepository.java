8341368f985587e1c9799563802aab0a1d9e9f8e
TechnoStu <nishars1854@gmail.com>
2025-06-21 20:18:31 -0400

    Removed

--- Assign_3_WebFlux_Nisha/Assignment3_WebFluxMono/src/main/java/com/assignment3/webfluxmono/nisha/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment3.webfluxmono.nisha.repository;
-
-import com.assignment3.webfluxmono.nisha.model.Member;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends ReactiveMongoRepository<Member, String> {
-}
--- Assign_3_WebFlux_Nisha/Assignment3_WebFluxMono/src/main/java/com/assignment3/webfluxmono/nisha/repository/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment3.webfluxmono.nisha.repository;
-
-import com.assignment3.webfluxmono.nisha.model.Publisher;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublisherRepository extends ReactiveMongoRepository<Publisher, String> {
-}
